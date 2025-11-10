9aacff679c02c0923548a4ff03a5cde723ee19b3
fd308-dev <fd308@georgetown.edu>
2025-11-10 18:33:46 -0500

    More changes

--- app/stocks.py
+++ app/stocks.py
@@ -1,34 +1,34 @@
 
-
 import os
 
-from dotenv import load_dotenv
 from pandas import read_csv
+from dotenv import load_dotenv
 import plotly.express as px
 
-load_dotenv() # read env vars from the ".env" file
+load_dotenv() # loads environment variables from the ".env" file
 
-ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
-
-
-def fetch_stocks_data(symbol):
-    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&outputsize=full&datatype=csv"
-    return read_csv(request_url)
+ALPHAVANTAGE_API_KEY= os.getenv("ALPHAVANTAGE_API_KEY")
 
 
-if __name__ == "__main__":
+def fetch_stocks_csv(symbol = "NFLX"):
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
+    stocks_df = read_csv(request_url)
+    return stocks_df
 
-    selected_symbol = input("Please input a stock symbol (e.g. 'GOOGL'): ") or "GOOGL"
-    df = fetch_stocks_data(selected_symbol)
-    print(df.head())
 
-    # todo: answer some questions about the data
-    latest_close = df["adjusted_close"][0]
-    print(f"LATEST CLOSE: ${latest_close:.2f}")
+if __name__=="__main__":
+    
+    #FETCH THE DATA
 
-    # make a dataviz:
-    title = f"Stock Prices over Time ({selected_symbol})"
-    fig = px.line(df, x="timestamp", y="adjusted_close",
-                  title=title, height=400,
-    )
-    fig.show()
+    symbol=input("Please choose a stock symbol: ") or "NFLX"
+
+    stocks_df =fetch_stocks_csv(symbol)
+    print(stocks_df.head())
+
+    # CHART THE DATA
+
+fig = px.line(stocks_df, x="timestamp", y="adjusted_close",
+              title=f"Stock Prices ({symbol})",
+              height=450
+              )
+fig.show()
